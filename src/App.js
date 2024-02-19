093d977c48c22e3d299aa4cd9651fe0bdc1a8c65
Gangadhar <gangadharsesetti43376@gmail.com>
2024-02-19 22:15:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+// src/components/App.js
+import React, { Component } from 'react';
 import './App.css';
+import { LiaFileImportSolid } from "react-icons/lia";
+import { FiArrowRightCircle } from "react-icons/fi";
+import { IoPrintOutline } from "react-icons/io5";
+import { LuRefreshCw } from "react-icons/lu";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   render() {
+        const orders = [
+            { channelImg: 'https://res.cloudinary.com/dhscilqq8/image/upload/v1708283355/download_bji4et.png', orderNo: '#IN123456789', orderDate: '2022-05-24', city: 'Lucknow', customerName: 'Abhinav Dixit', orderValue: 8.00, status: 'Pending' },
+            { channelImg: 'https://res.cloudinary.com/dhscilqq8/image/upload/v1708283355/download_bji4et.png', orderNo: '#IN123456789', orderDate: '2022-05-24', city: 'Lucknow', customerName: 'Abhinav Dixit', orderValue: 8.00, status: 'Pending' },
+            { channelImg: 'https://res.cloudinary.com/dhscilqq8/image/upload/v1708283355/download_bji4et.png', orderNo: '#IN123456789', orderDate: '2022-05-24', city: 'Lucknow', customerName: 'Abhinav Dixit', orderValue: 8.00, status: 'Pending' },
+            { channelImg: 'https://res.cloudinary.com/dhscilqq8/image/upload/v1708283355/download_bji4et.png', orderNo: '#IN123456789', orderDate: '2022-05-24', city: 'Lucknow', customerName: 'Abhinav Dixit', orderValue: 8.00, status: 'Pending' },
+        ];
+
+        return (
+            <div>
+                <div className="new-navbar">
+                    <h1>Pending</h1>
+                    <h1>Accepted</h1>
+                    <h1>AWB Created</h1>
+                    <h1>Ready to Ship</h1>
+                    <h1>Shipped</h1>
+                    <h1>Completed</h1>
+                    <h1>Cancelled</h1>
+                </div> 
+                <div className='container'>
+                  <div className='inside-container'>
+                    <div className='box'><LiaFileImportSolid /> Import Orders</div>
+                    <div className='box23'><FiArrowRightCircle/> Accept</div>
+                    <div className='box23'><IoPrintOutline/>Print</div>
+                  </div>
+                  <button><LuRefreshCw/>Refresh</button>
+                </div>
+                <div className="order-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th>Channel</th>
+                                <th>Order No.</th>
+                                <th>Order Date</th>
+                                <th>City</th>
+                                <th>Customer Name</th>
+                                <th>Order Value</th>
+                                <th>Status</th>
+                                <th>Operation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orders.map((order, index) => (
+                                <tr key={index}>
+                                    <td>+</td>
+                                    <td>
+                                        <input type="checkbox"/>
+                                    </td>
+                                    <td>
+                                        <img src={order.channelImg} alt="Channel Logo"  />
+                                    </td>
+                                    <td>{order.orderNo}</td>
+                                    <td>{order.orderDate}</td>
+                                    <td>{order.city}</td>
+                                    <td>{order.customerName}</td>
+                                    <td>{order.orderValue}</td>
+                                    <td>{order.status}</td>
+                                    <td>
+                                        <select>
+                                            <option value="edit">Action</option>
+                                            <option value="delete">None</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
